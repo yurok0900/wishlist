b1faaab6013f057e067aeaec858ce59bc0ca2013
yurok0900 <83572024+yurok0900@users.noreply.github.com>
2021-09-10 22:12:15 +0300

    add about.html and change index.html

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index
+from main.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index_page'),
+    path('about/', about, name='about_page'),
 ]
