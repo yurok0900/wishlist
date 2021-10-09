385239f0c2df2619ec6912f5ec70c0bb43f210c8
yurok0900 <83572024+yurok0900@users.noreply.github.com>
2021-10-09 16:29:23 +0300

    add button and function

--- main/models.py
+++ main/models.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    """ Таблица "Товаров"
-    
+    """ Таблица "Товаров" 
     
     id
     Название товара
     Ссылка на товар
     Цена на товар
-    Дата и время создания записи    
+    Дата и время создания записи  
+    
     """
     title = models.CharField(max_length=120)
     link = models.URLField()
     price = models.IntegerField()
-    create_at = models.DateTimeField(auto_created=True)
+    create_at = models.DateTimeField(
+        auto_now_add=True,
+        auto_created=True
+        )
     
     def __str__(self):
         return self.title
@@ -22,12 +25,12 @@ class Product(models.Model):
 class WishList(models.Model):
     """ Таблица "Список желаемых подарков"
     
-    
     id
     Название
     Владелец (Пользователь)
     Товары - ManyToMany
     Скрыт ли - bool
+    
     """
     title = models.CharField(max_length=120)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import WishList
+from .forms import ProductForm
+
 
 def index(request):
     return render(request,'index.html',{})
@@ -9,12 +11,25 @@ def about(request):
     return render(request,'about.html',{})
 
 def list_page(request, pk):
+    """
+    View page the wishlist.
+    """
     wishlist = get_object_or_404(WishList, pk=pk)
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        product = form.save()
+        wishlist.product.add(product)
+        wishlist.save()
+    elif request.method == 'GET':
+        form = ProductForm
+
     return render(
         request,
         'wishlist.html',
         {
-            'wishlist':wishlist,
+            'wishlist': wishlist,
             'is_owner_list': wishlist.owner == request.user,
+            'form': form,
         }
     )
